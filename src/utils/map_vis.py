6cbe6893c90700e065d83968d2c22c119f870b51
JHWu92 <jhwu92@gmail.com>
2018-10-02 14:55:49 -0400

    map vis

---
+++ src/utils/map_vis.py
@@ -0,0 +1,19 @@
+﻿import folium
+
+def geojson_per_row(gpdf, name, color='blue', tip_cols=None, some_map=None):
+
+    feature_group = folium.FeatureGroup(name=name)
+    for row in gpdf.itertuples():
+
+        if row.geometry.boundary.type=='MultiLineString':
+            lines = row.geometry.boundary
+        else:
+            lines= [row.geometry.boundary]
+            
+        for line in lines:
+            print
+            tip = '<br>'.join(['%s: %s' % (col, getattr(row, col)) for col in tip_cols]) if tip_cols is not None else name
+            folium.Polygon(locations=[(lat,lon) for lon,lat in line.coords], color=color, fill_color=color, tooltip=tip, popup=tip).add_to(feature_group)
+    if some_map is not None:
+        feature_group.add_to(some_map)
+    return feature_group
