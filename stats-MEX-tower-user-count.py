ef0b4d9142f7ab550d29f9f4d3bbaa05d8d0ac0b
JHWu92 <jhwu92@gmail.com>
2018-11-06 20:53:19 -0500

    catch EOFError, json+gz saving file

--- stats-MEX-tower-user-count.py
+++ stats-MEX-tower-user-count.py
@@ -1,18 +1,24 @@
 # coding: utf-8
-
 import datetime
 import glob
 import gzip
+import json
 import logging
 import os
 from collections import defaultdict
 
-import pandas as pd
-
 from src.creds import drive_root, mex_col_call
 
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
 debugging = False
-include_in_calls = False
+voz_only = True
 
 level = logging.DEBUG if debugging else logging.INFO
 logging.basicConfig(filename="logs/MEX-tower-user-count.log", level=level,
@@ -26,27 +32,28 @@ idx_duration = mex_col_call.index('duration')
 idx_p1 = mex_col_call.index('phone_1')
 
 mex_root = drive_root + "MEX/"
-file_pattern = '??????/*TRAFICO_VOZ*' if include_in_calls else '??????/*TRAFICO_VOZ_[0-9]*'
-stats_dir = 'stats/MEX-tower-user-count-w-in-calls/' if include_in_calls else 'stats/MEX-tower-user-count/'
-os.makedirs(stats_dir, exist_ok=True)
-
+file_pattern = '??????/*TRAFICO_VOZ_[0-9]*' if voz_only else '??????/*TRAFICO_VOZ_ENTRANTE*'
 fns = []
 for fn in glob.glob(mex_root + file_pattern):
     fns.append(fn)
+fns = sorted(fns)
+
+stats_dir = 'stats/MexTwUniqUser-VOZ/' if voz_only else 'stats/MexTwUniqUser-VOZENTRANTE/'
+if debugging: stats_dir += 'debug/'
+os.makedirs(stats_dir, exist_ok=True)
 
 # loop over the files
 start_dt = datetime.datetime.now()
 logging.info('===============================')
-logging.info('MEX stats starts. debugging=%s, including incoming calls=%s' % (debugging, include_in_calls))
+logging.info('MEX stats starts. Number of files: %d, debugging=%s, VOZ_only=%s' % (len(fns), debugging, voz_only))
 
 for cnt, fn in enumerate(fns):
-    if cnt % 30 ==0:
-        print('working on the %dth file' % cnt)
+    print('working on the %dth file' % cnt)
 
     logging.info('processing file: %s' % fn.replace(drive_root, ''))
-    file_date = fn.replace('.dat','').replace('.gz','')[-8:]
+    file_date = fn.replace('.dat', '').replace('.gz', '')[-8:]
     file_date = datetime.datetime.strptime(file_date, '%Y%m%d')
-    file_date = file_date.strftime('%d-%m-%Y')
+    file_date = file_date.strftime('%Y-%m-%d')
 
     # for logging processing time per file
     fn_start_dt = datetime.datetime.now()
@@ -56,43 +63,33 @@ for cnt, fn in enumerate(fns):
 
     # loop over the lines in a file
     # some files are not gzipped
-    f = gzip.open(fn, 'rb') if fn.endswith('.gz') else open(fn, 'rb')
+    try:
+        f = gzip.open(fn, 'rb') if fn.endswith('.gz') else open(fn, 'rb')
 
-    for i, line in enumerate(f):
-        if i > 10 and debugging:
-            break
+        for i, line in enumerate(f):
+            if i > 10 and debugging:
+                break
 
-        line = line.decode('utf8').strip().split('|')
+            line = line.decode('utf8').strip().split('|')
 
-        dt1 = datetime.datetime.strptime(line[idx_date] + ' ' + line[idx_time], '%d/%m/%Y %H:%M:%S')
-        dt2 = dt1 + datetime.timedelta(seconds=int(line[idx_duration]))
+            dt1 = datetime.datetime.strptime(line[idx_date] + ' ' + line[idx_time], '%d/%m/%Y %H:%M:%S')
+            dt2 = dt1 + datetime.timedelta(seconds=int(line[idx_duration]))
 
-        d1 = dt1.strftime('%d-%m-%Y')
-        d1h = dt1.hour
-        d2 = dt2.strftime('%d-%m-%Y')
-        d2h = dt2.hour
+            d1 = dt1.strftime('%Y-%m-%d')
+            d1h = dt1.hour
+            d2 = dt2.strftime('%Y-%m-%d')
+            d2h = dt2.hour
 
-        stats[d1][line[idx_t1]][d1h].add(line[idx_p1])
-        stats[d2][line[idx_t2]][d2h].add(line[idx_p1])
+            stats[d1][line[idx_t1]][d1h].add(line[idx_p1])
+            stats[d2][line[idx_t2]][d2h].add(line[idx_p1])
+        logging.debug('iterated all lines')
+        f.close()
+    except EOFError as e:
+        logging.exception('file %s raise EOFError' % fn)
 
-    logging.debug('iterated all lines')
-    f.close()
-
-    # saving the stats
-    if file_date in stats:
-        logging.debug('file_date in the stats, saving csv')
-        today = pd.DataFrame.from_dict(stats.pop(file_date)).applymap(lambda x: pd.np.nan if pd.isnull(x) else len(x))
-        today.reindex(list(range(24))).to_csv(stats_dir + '%s.csv' % file_date)
-    else:
-        logging.warning('file_date not in the stats')
-
-    # there are more dates in addition to file_date
-    if len(stats) != 0:
-        extra_dates = list(stats.keys())
-        logging.warning('There are dates in addition to the file_date: %s' % extra_dates)
-        for ed in extra_dates:
-            extra = pd.DataFrame.from_dict(stats[ed]).applymap(lambda x: pd.np.nan if pd.isnull(x) else len(x))
-            extra.reindex(list(range(24))).to_csv(stats_dir + '%s-from-%s.csv' % (ed, file_date))
+    # save file
+    with gzip.open(stats_dir + '%s.json.gz' % file_date, 'wt') as zipfile:
+        json.dump(stats, zipfile, cls=SetEncoder)
 
     logging.info('File processing time: %f seconds' % (datetime.datetime.now() - fn_start_dt).total_seconds())
 
