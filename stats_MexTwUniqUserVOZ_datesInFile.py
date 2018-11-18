3ca4d0e4bf9a625b68002841325a325b107a6016
JHWu92 <jhwu92@gmail.com>
2018-11-17 19:55:52 -0500

    datesInFile
    Could've done with MexTwUniqUserVOZ

---
+++ stats_MexTwUniqUserVOZ_datesInFile.py
@@ -0,0 +1,17 @@
+import glob
+import gzip
+import json
+import os
+from collections import defaultdict
+
+dates = defaultdict(list)
+
+for fn in glob.glob('stats/MexTwUniqUserVOZ/*.json.gz'):
+    print(fn)
+    with gzip.open(fn) as fin:
+        data = json.load(fin)
+        for d in data.keys():
+            dates[d].append(os.path.basename(fn).replace('.json.gz', ''))
+
+with open('stats/MexTwUniqUserVOZ/dates_in_file.json', 'w') as fw:
+    json.dump(dates, fw)
