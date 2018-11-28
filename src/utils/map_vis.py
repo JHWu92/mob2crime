d47c4cab7b979c61bb009e446eea45c0889d1129
JHWu92 <jhwu92@gmail.com>
2018-11-28 17:04:44 -0500

    point per row

--- src/utils/map_vis.py
+++ src/utils/map_vis.py
@@ -1,19 +1,30 @@
 ﻿import folium
 
-def geojson_per_row(gpdf, name, color='blue', tip_cols=None, some_map=None):
 
+def geojson_per_row(gpdf, name, color='blue', tip_cols=None, some_map=None):
     feature_group = folium.FeatureGroup(name=name)
     for row in gpdf.itertuples():
 
-        if row.geometry.boundary.type=='MultiLineString':
+        if row.geometry.boundary.type == 'MultiLineString':
             lines = row.geometry.boundary
         else:
-            lines= [row.geometry.boundary]
-            
+            lines = [row.geometry.boundary]
+
         for line in lines:
-            print
-            tip = '<br>'.join(['%s: %s' % (col, getattr(row, col)) for col in tip_cols]) if tip_cols is not None else name
-            folium.Polygon(locations=[(lat,lon) for lon,lat in line.coords], color=color, fill_color=color, tooltip=tip, popup=tip).add_to(feature_group)
+            tip = '<br>'.join(
+                ['%s: %s' % (col, getattr(row, col)) for col in tip_cols]) if tip_cols is not None else name
+            folium.Polygon(locations=[(lat, lon) for lon, lat in line.coords], color=color, fill_color=color,
+                           tooltip=tip, popup=tip).add_to(feature_group)
+    if some_map is not None:
+        feature_group.add_to(some_map)
+    return feature_group
+
+
+def point_per_row(gpdf, name, tip_cols=None, some_map=None):
+    feature_group = folium.FeatureGroup(name=name)
+    for row in gpdf.itertuples():
+        tip = '<br>'.join(['%s: %s' % (col, getattr(row, col)) for col in tip_cols]) if tip_cols is not None else name
+        folium.Marker((row.geometry.y, row.geometry.x), tooltip=tip, popup=tip).add_to(feature_group)
     if some_map is not None:
         feature_group.add_to(some_map)
     return feature_group
