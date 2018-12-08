1c5e9c6205505520798aa521e16faa8e2ec0f3e4
JHWu92 <jhwu92@gmail.com>
2018-12-08 11:52:23 -0500

    add no t1&2 stats

--- DQ_CDR_stats_MexNoTwCallPcnt.py
+++ DQ_CDR_stats_MexNoTwCallPcnt.py
@@ -14,17 +14,19 @@ def sum_count(d):
     a = 0
     nt1 = 0
     nt2 = 0
+    nt12 = 0
     for k, v in d.items():
         a += v.get('all', 0)
         nt1 += v.get('nt1', 0)
         nt2 += v.get('nt2', 0)
-    return a, nt1, nt2
+        nt12 += v.get('nt12', 0)
+    return a, nt1, nt2, nt12
 
 
 sd = datetime.datetime.now()
 print('stated', sd)
 voz_or_en = 'VOZ'
-stats_dir = 'stats/AggMexUsrNoTwCall%s/' % voz_or_en
+stats_dir = 'stats/DQAggMexUsrNoTwCall%s/' % voz_or_en
 
 fns = sorted(list(glob.glob(stats_dir + '*.json.gz')))
 num_calls = {}
@@ -36,14 +38,17 @@ for i, fn in enumerate(fns):
         data = json.load(fin)
         num_calls[date] = sum_count(data)
 
-df = pd.DataFrame(list(num_calls.values()), index=list(num_calls.keys()), columns=['all', 'nt1', 'nt2'])
+df = pd.DataFrame(list(num_calls.values()), index=list(num_calls.keys()), columns=['all', 'nt1', 'nt2', 'nt12'])
 df['nt1pct'] = df.nt1 / df['all']
 df['nt2pct'] = df.nt2 / df['all']
+df['nt12pct'] = df.nt12 / df['all']
 nt1p = 100 * df.nt1.sum() / df['all'].sum()
 nt2p = 100 * df.nt2.sum() / df['all'].sum()
+nt12p = 100 * df.nt12.sum() / df['all'].sum()
 df.to_csv('stats/MexNoTwCallPcntDaily.csv')
 
-ax = df[['nt1pct', 'nt2pct']].plot.hist(alpha=0.5, title='overall no t1=%0.2f%% and no t2=%0.2f%%' % (nt1p, nt2p))
+ax = df[['nt1pct', 'nt2pct', 'nt12pct']].plot.hist(
+    alpha=0.5, title='overall no t1=%0.2f%% and no t2=%0.2f%% and no t1&2=%0.2f%%' % (nt1p, nt2p, nt12p))
 fig = ax.get_figure()
 fig.savefig('stats/MexNoTwCallPcntDailyHist.png')
 
