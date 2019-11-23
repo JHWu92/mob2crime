c6e7c6660677f1d8862f69f81064171abdf9fbe4
JWu <jhwu92@gmail.com>
2019-11-22 22:34:05 -0500

    numba to optimize

--- src/ftrs/compactness_index.py
+++ src/ftrs/compactness_index.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numba as nb
 
 
 def compacity_coefficient(pairwise_distance_average, sqrt_area):
@@ -27,23 +28,8 @@ def moment_inertia(raster_areas, d2centroid):
     return nmi
 
 
-def mass_moment_inertia(raster_rper, d2mass_centroid):
-    # for raster representation, area of each grid are the same
-    if raster_rper.Area.nunique() != 1:
-        area = raster_rper.Area
-        aiaj = np.outer(raster_rper.Area.values, raster_rper.Area.values)
-    else:
-        area = raster_rper.Area.unique()[0]
-        aiaj = area ** 2
-
-    # mass moment of inertia
-    # IGs = density * IG (the mi of the grid to its own centroid)
-    # md2s = distance square to mass centroid * its own mass
-    # mmi = sum of IGs+md2s
-    IGs = raster_rper.Density * area ** 2 / 6
-    md2s = raster_rper.Mass * d2mass_centroid ** 2
-    mmi = (IGs + md2s).sum()
-
+# slow and memory consuming
+def get_sum_min_pij_aij(raster_rper, aiaj):
     density = raster_rper.Density
     di = density[np.newaxis, :]
     dj = density[:, np.newaxis]
@@ -51,7 +37,55 @@ def mass_moment_inertia(raster_rper, d2mass_centroid):
     min_pij_aij = min_pij_aij * di + (1 - min_pij_aij) * dj
     min_pij_aij = 2 * min_pij_aij * aiaj
     sum_min_pij_aij = (min_pij_aij.sum() - np.trace(min_pij_aij).sum()) / 2
-    density_area2 = (density * area ** 2).sum()
-    mmi_ref = (density_area2 + sum_min_pij_aij) / 2 / np.pi
+    return sum_min_pij_aij
+
+
+@numba.njit
+def compute_sum_2_min_pipj_aiaj(density_arr, aiaj):
+    length = len(density_arr)
+    s = 0
+    for i in range(length):
+        pi = density_arr[i]
+        for j in range(i + 1, length):
+            s += 2 * min(pi, density_arr[j]) * aiaj
+    return s
+
+
+@numba.njit
+def compute_sum_2_min_pipj_aiaj_with_area_arr(density_arr, area_arr):
+    length = len(density_arr)
+    s = 0
+    for i in range(length):
+        pi = density_arr[i]
+        ai = area_arr[i]
+        for j in range(i + 1, length):
+            s += 2 * min(pi, density_arr[j]) * ai * area_arr[j]
+    return s
+
+
+
+def mass_moment_inertia(raster_rper, d2mass_centroid):
+    density_arr = raster_rper.Density.values
+    if raster_rper.Area.nunique() != 1:
+        area = raster_rper.Area
+        aiaj = raster_rper.Area.values
+        pipj_aiaj_func = compute_sum_2_min_pipj_aiaj_with_area_arr
+    else:
+        # for raster representation, area of each grid are the same
+        area = raster_rper.Area.unique()[0]
+        aiaj = area ** 2
+        pipj_aiaj_func = compute_sum_2_min_pipj_aiaj
+
+    # mass moment of inertia
+    # IGs = density * IG (the mi of the grid to its own centroid)
+    # md2s = distance square to mass centroid * its own mass
+    # mmi = sum of IGs+md2s
+    IGs = (density_arr * area ** 2 / 6).sum()
+    md2s = (raster_rper.Mass * d2mass_centroid ** 2).sum()
+    mmi = IGs + md2s
+
+    density_area_square = (density_arr * area ** 2).sum()
+    sum_2_min_pij_aij = pipj_aiaj_func(density_arr, aiaj)
+    mmi_ref = (density_area_square + sum_2_min_pij_aij) / 2 / np.pi
     nmmi = mmi_ref / mmi
     return nmmi
