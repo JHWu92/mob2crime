bcec0d5c30cff4e9d6f19c900946a0ea884964b4
JHWu92 <jhwu92@gmail.com>
2020-02-09 11:49:01 -0500

    Update pre_compute_features.py

--- pre_compute_features.py
+++ pre_compute_features.py
@@ -28,21 +28,21 @@ def get_su(su_type, admin_lvl, admin_id, admin_shape, crs=None):
     return su
 
 
-def get_hotspots_per_hour(towers_vor, su, tw_footfall, intpl_type, cache_path=None, verbose=0):
+def get_su_footfall(towers_vor, su, tw_footfall, intpl, cache_path=None, verbose=0):
     """
     return interpolated footfall for spatial untis
     :param towers_vor: tower vor polygons
     :param su: spatial units
     :param tw_footfall: footfall for towers
-    :param intpl_type: type of interpolation method
+    :param intpl: type of interpolation method
     :param cache_path: if not None, cahce t2su results, used by specific interpolation function
     :param verbose:
     :return:
     """
-    if intpl_type == 'Uni':
+    if intpl == 'Uni':
         su_footfall = tw_int.interpolate_uni(towers_vor, su, tw_footfall, cache_path, verbose)
     else:
-        raise NotImplementedError(f'intpl_type="{intpl_type}" has not been implemented')
+        raise NotImplementedError(f'intpl="{intpl}" has not been implemented')
 
     return su_footfall
 
@@ -56,7 +56,7 @@ def main():
     boundary_type = 'Urban'
     urb_only = {'Urban': True, 'UrbanRural': False}[boundary_type]
     su_type = 'grid-500'
-    intpl_type = 'Uni'
+    intpl = 'Uni'
     # footfall hotspots related
     hs_types = ['loubar', 'average'][:1]
     wd_wk = 'wd'  # average weekdays or weekends
@@ -87,28 +87,29 @@ def main():
     tw_footfall = pd.DataFrame(aver[wd_wk]).T
 
     redo = False
-    for mid, munic in mgm.iterrows():
+    for i, (mid, munic) in enumerate(mgm.iterrows()):
+        # if i > 5: break
         admin_id = mid
-        print(f'====== working on {admin_lvl}={admin_id}, {datetime.datetime.now()}', end='')
+        print(f'====== working on {i}, {admin_lvl}={admin_id}, {datetime.datetime.now()}', end='')
         city_area = munic.geometry.area
         su = get_su(su_type, admin_lvl, mid, munic.geometry, mex_crs)
         tid_oi = towers_vor_in_mgm[towers_vor_in_mgm.mun_id == mid].gtid
 
-        su_footfall = get_hotspots_per_hour(towers_vor.loc[tid_oi], su,
-                                            tw_footfall.reindex(tid_oi, fill_value=0),
-                                            intpl_type)
+        su_footfall = get_su_footfall(towers_vor.loc[tid_oi], su,
+                                      tw_footfall.reindex(tid_oi, fill_value=0),
+                                      intpl)
         for hotspot_type in hs_types:
             hotspots_per_hour = ftr_hs.keep_hotspot(su_footfall.copy(), hotspot_type)
 
-            ftr_gen.ftr_hs_scale(db, country, admin_lvl, admin_id, boundary_type, su_type, intpl_type,
+            ftr_gen.ftr_hs_scale(db, country, admin_lvl, admin_id, boundary_type, su_type, intpl,
                                  hotspot_type=hotspot_type, hotspots_per_hour=hotspots_per_hour,
                                  su=su, redo=redo)
 
-            ftr_gen.ftr_compacity(db, country, admin_lvl, admin_id, boundary_type, su_type, intpl_type,
+            ftr_gen.ftr_compacity(db, country, admin_lvl, admin_id, boundary_type, su_type, intpl,
                                   hotspot_type=hotspot_type, hotspots_per_hour=hotspots_per_hour,
                                   su=su, city_area=city_area, redo=redo)
 
-            ftr_gen.ftr_compactness(db, country, admin_lvl, admin_id, boundary_type, su_type, intpl_type,
+            ftr_gen.ftr_compactness(db, country, admin_lvl, admin_id, boundary_type, su_type, intpl,
                                     hotspot_type=hotspot_type, raster_resolution=raster_resolution,
                                     raster_use_p_centroid_if_none=raster_use_p_centroid_if_none,
                                     su=su, hotspots_per_hour=hotspots_per_hour, redo=redo, verbose=0)
