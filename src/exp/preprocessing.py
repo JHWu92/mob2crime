12a635dea94557b024a63263f5d01b620c09c5ff
JHWu92 <jhwu92@gmail.com>
2020-02-18 19:51:46 -0500

    Update preprocessing.py

--- src/exp/preprocessing.py
+++ src/exp/preprocessing.py
@@ -1,6 +1,24 @@
 import sklearn.preprocessing as sk_prep
 
 
+def train_test_split(X, y, random_state=0):
+    train_X = X.sample(frac=0.8, random_state=random_state)
+    train_y = y.loc[train_X.index]
+    test_index = sorted(set(X.index) - set(train_X.index))
+    test_X = X.loc[test_index]
+    test_y = y.loc[test_index]
+    return train_X, test_X, train_y, test_y
+
+
+def fillna(features):
+    for col in features.columns:
+        if col.split('-')[1] in ['COMP', 'MCOMP', 'COHE', 'PROX', 'NMI', 'NMMI']:
+            # NaN in these features means there isn't any hotspots, hence no feature is computed.
+            # here I use 0 for NaN
+            features[col] = features[col].fillna(0)
+    return features
+
+
 def scaling_data(train_x, test_x, scaling='RobustScaler'):
     if isinstance(scaling, str):
         assert scaling in ('StandardScaler', 'MinMaxScaler', 'RobustScaler')
